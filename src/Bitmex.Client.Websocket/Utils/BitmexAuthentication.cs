6598191ab0a2b99a32c115949bb96ac28811be28
ColossusFX <contact@iamaflip.co.uk>
2018-07-07 09:08:30 +0100

    Update BitmexAuthentication.cs
    Removed nonce
    Api Expires added

--- src/Bitmex.Client.Websocket/Utils/BitmexAuthentication.cs
+++ src/Bitmex.Client.Websocket/Utils/BitmexAuthentication.cs
@@ -3,37 +3,35 @@ using System.Text;
 
 namespace Bitmex.Client.Websocket.Utils
 {
-    public static class BitmexAuthentication
-    {
-        public static string BitmexUrl()
-        {
+	public static class BitmexAuthentication
+	{
+		public static string BitmexUrl()
+		{
 			return "GET/realtime";
 		}
 
-        public static string CreateSignature(string key, string message)
-        {
-            var keyBytes = Encoding.UTF8.GetBytes(key);
-            var messageBytes = Encoding.UTF8.GetBytes(message);
+		public static string CreateSignature(string key, string message)
+		{
+			var keyBytes = Encoding.UTF8.GetBytes(key);
+			var messageBytes = Encoding.UTF8.GetBytes(message);
 
 
-            string ByteToString(byte[] buff)
-            {
-                var builder = new StringBuilder();
+			string ByteToString(byte[] buff)
+			{
+				var builder = new StringBuilder();
 
-                for (var i = 0; i < buff.Length; i++)
-                {
-                    builder.Append(buff[i].ToString("X2")); // hex format
-                }
-                return builder.ToString();
-            }
+				for (var i = 0; i < buff.Length; i++)
+				{
+					builder.Append(buff[i].ToString("X2")); // hex format
+				}
+				return builder.ToString();
+			}
 
-            using (var hmacsha256 = new HMACSHA256(keyBytes))
-            {
-                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
-                return ByteToString(hashmessage).ToLower();
-            }
-        }
-
-	    
+			using (var hmacsha256 = new HMACSHA256(keyBytes))
+			{
+				byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+				return ByteToString(hashmessage).ToLower();
+			}
+		}
 	}
 }
