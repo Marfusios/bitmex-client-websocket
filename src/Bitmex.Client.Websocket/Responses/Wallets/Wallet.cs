ec8140165ff850ea7e97a60aa8c2c7b85c22eaad
Mariusz Kotas <m@mkotas.cz>
2019-02-12 10:08:18 +0100

    Bitmex converter - more comments, provide also decimal methods.

--- src/Bitmex.Client.Websocket/Responses/Wallets/Wallet.cs
+++ src/Bitmex.Client.Websocket/Responses/Wallets/Wallet.cs
@@ -4,10 +4,20 @@ using Bitmex.Client.Websocket.Utils;
 
 namespace Bitmex.Client.Websocket.Responses.Wallets
 {
+    /// <summary>
+    /// Information about your wallet (balance, changes, address, etc)
+    /// </summary>
     [DebuggerDisplay("Wallet: {Currency} - {BalanceBtc}")]
     public class Wallet
     {
+        /// <summary>
+        /// Account identification
+        /// </summary>
         public long Account { get; set; }
+
+        /// <summary>
+        /// Current `Amount` currency, for example `XBt` which is satoshi
+        /// </summary>
         public string Currency { get; set; }
 
         public long? PrevDeposited { get; set; }
@@ -15,9 +25,16 @@ namespace Bitmex.Client.Websocket.Responses.Wallets
         public long? PrevTransferIn { get; set; }
         public long? PrevTransferOut { get; set; }
         public long? PrevAmount { get; set; }
+
         public long? TransferIn { get; set; }
         public long? TransferOut { get; set; }
+
+        /// <summary>
+        /// Current balance in satoshis (call `BalanceBtc` property to get BTC representation)
+        /// </summary>
         public long? Amount { get; set; }
+
+
         public long? PendingCredit { get; set; }
         public long? PendingDebit { get; set; }
         public long? ConfirmedDebit { get; set; }
@@ -27,6 +44,14 @@ namespace Bitmex.Client.Websocket.Responses.Wallets
         public string Script { get; set; }
         public string[] WithdrawalLock {get; set; }
 
+        /// <summary>
+        /// Converted satoshis 'Amount' into double BTC representation
+        /// </summary>
         public double BalanceBtc => BitmexConverter.ConvertToBtc(Currency, Amount ?? 0);
+
+        /// <summary>
+        /// Converted satoshis 'Amount' into decimal BTC representation
+        /// </summary>
+        public decimal BalanceBtcDecimal => BitmexConverter.ConvertToBtcDecimal(Currency, Amount ?? 0);
     }
 }
--- src/Bitmex.Client.Websocket/Utils/BitmexConverter.cs
+++ src/Bitmex.Client.Websocket/Utils/BitmexConverter.cs
@@ -3,8 +3,18 @@ using Bitmex.Client.Websocket.Exceptions;
 
 namespace Bitmex.Client.Websocket.Utils
 {
+    /// <summary>
+    /// Utility class to help with converting Satoshis, miliBTC into BTC
+    /// </summary>
     public static class BitmexConverter
     {
+        /// <summary>
+        /// Convert double value into BTC, according to from currency.
+        /// For example: 'XBt' will be converted like value/10^8
+        /// </summary>
+        /// <param name="from">Currency (XBt, XBT, BTC, etc)</param>
+        /// <param name="value">Original value</param>
+        /// <returns>Converted value</returns>
         public static double ConvertToBtc(string from, double value)
         {
             var safe = (from ?? string.Empty).Trim();
@@ -18,23 +28,88 @@ namespace Bitmex.Client.Websocket.Utils
             if (string.IsNullOrWhiteSpace(safe) || safe.ToLower() == "btc" || safe.ToLower() == "xbt")
                 return value;
 
-            throw new BitmexException($"Can't convert from '{safe}' to BTC");
+            throw new BitmexException($"Can't convert from '{safe}' to BTC (double)");
         }
 
+        /// <summary>
+        /// Convert decimal value into BTC, according to from currency.
+        /// For example: 'XBt' will be converted like value/10^8
+        /// </summary>
+        /// <param name="from">Currency (XBt, XBT, BTC, etc)</param>
+        /// <param name="value">Original value</param>
+        /// <returns>Converted value</returns>
+        public static decimal ConvertToBtc(string from, decimal value)
+        {
+            var safe = (from ?? string.Empty).Trim();
+
+
+            if (safe == "XBt")
+            {
+                return ConvertFromSatoshiToBtcDecimal(value);
+            }
+
+            if (string.IsNullOrWhiteSpace(safe) || safe.ToLower() == "btc" || safe.ToLower() == "xbt")
+                return value;
+
+            throw new BitmexException($"Can't convert from '{safe}' to BTC (decimal)");
+        }
+
+        /// <summary>
+        /// Convert long value into BTC, according to from currency.
+        /// For example: 'XBt' will be converted like value/10^8
+        /// </summary>
+        /// <param name="from">Currency (XBt, XBT, BTC, etc)</param>
+        /// <param name="value">Original value</param>
+        /// <returns>Converted value</returns>
         public static double ConvertToBtc(string from, long value)
         {
             var valueDouble = Convert.ToDouble(value);
             return ConvertToBtc(from, valueDouble);
         }
 
+        /// <summary>
+        /// Convert long value into BTC, according to from currency.
+        /// For example: 'XBt' will be converted like value/10^8
+        /// </summary>
+        /// <param name="from">Currency (XBt, XBT, BTC, etc)</param>
+        /// <param name="value">Original value</param>
+        /// <returns>Converted value</returns>
+        public static decimal ConvertToBtcDecimal(string from, long value)
+        {
+            var valueDouble = Convert.ToDecimal(value);
+            return ConvertToBtc(from, valueDouble);
+        }
+
+        /// <summary>
+        /// Convert Satoshis into BTC. It will divide value by 10^8
+        /// </summary>
         public static double ConvertFromSatoshiToBtc(long satoshi)
         {
             return satoshi / 100000000.0;
         }
 
+        /// <summary>
+        /// Convert Satoshis into BTC. It will divide value by 10^8
+        /// </summary>
         public static double ConvertFromSatoshiToBtc(double satoshi)
         {
             return satoshi / 100000000.0;
+        }
+
+        /// <summary>
+        /// Convert Satoshis into BTC. It will divide value by 10^8
+        /// </summary>
+        public static decimal ConvertFromSatoshiToBtcDecimal(long satoshi)
+        {
+            return satoshi / 100000000.0m;
+        }
+
+        /// <summary>
+        /// Convert Satoshis into BTC. It will divide value by 10^8
+        /// </summary>
+        public static decimal ConvertFromSatoshiToBtcDecimal(decimal satoshi)
+        {
+            return satoshi / 100000000.0m;
         }
     }
 }
